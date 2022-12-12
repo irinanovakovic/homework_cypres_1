ba64c8f85f3f09dc6da4a9f9b2b77c0ca4918b30
irinanovakovic <irina.karanovic@gmail.com>
2022-12-12 14:47:11 +0100

    Added new negative case

--- cypress/e2e/register.cy.js
+++ cypress/e2e/register.cy.js
@@ -1,5 +1,26 @@
-describe('empty spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('Register page - Negative cases', () => {
+
+
+  const personOne = {
+    firstName: "irina",
+    lastName:'Novakovic',
+    email:'irina.novakovic111@gmail.com',
+    password:'12345678',
+    confirmedPassword:'12345678',
+    terms:true
+  }
+
+  it('Checkout button is not confirmed', () => {
+    cy.visit('https://gallery-app.vivifyideas.com/register');
+
+    cy.get('#first-name').type(personOne.firstName)
+    cy.get('#last-name').type(personOne.lastName)
+    cy.get('#email').type(personOne.email)
+    cy.get('#password').type(personOne.password)
+    cy.get('#password-confirmation').type(personOne.confirmedPassword)
+    cy.get('.btn').click()
+
+    cy.contains('The terms and conditions must be accepted.').should('be.visible') 
+
   })
 })
